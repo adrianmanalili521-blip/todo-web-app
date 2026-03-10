a902fe92af4b7e1158729bc5cf43010d2d7c03f1
Adrian <adrianmanalili521@gmail.com>
2026-03-10 19:17:50 +0800

    removed height utility class in main div

--- frontend/my-project/src/App.tsx
+++ frontend/my-project/src/App.tsx
@@ -3,7 +3,7 @@ import PostTasks from './components/postTask'
 
 function App () {
     return (
-        <div className="display: h-screen w-200 flex flex-row content-between p-5 m-10 gap-10 bg-amber-50">
+        <div className="display: w-200 flex flex-row content-between p-5 m-10 gap-10 bg-amber-50">
             <div className="border rounded-2xl w-100 flex flex-col items-center">
                 Your Todo
                 <div className="flex flex-col items-start w-full p-2 bg">
